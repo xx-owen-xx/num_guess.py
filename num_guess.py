cfa72e00ff7ef3e9a7465872c3d689d3eb102dce
xx-owen-xx <fisselowen@gmail.com>
2025-07-05 17:32:13 -0700

    Add files via upload

---
+++ num_guess.py
@@ -0,0 +1,34 @@
+# Owen Fissel
+# ENG 103
+#num_guess.py
+
+
+# prompt user for integer to be guessed
+goal = int(input('Enter the integer for the player to guess. '))
+
+# guess counter
+
+guess_count = 0
+
+#prompt user for first guess
+
+guess = int(input('Enter your guess.\n'))
+guess_count += 1
+
+#loop untill goal
+
+while guess!= goal:
+    if guess > goal:
+        guess = int(input('Too hight - try again:\n'))
+    else:
+        guess = int(input('Too low - try again:\n'))
+    guess_count += 1
+    
+#print results
+if guess_count == 1:
+    print(' You guess it in 1 try.')
+else:
+    print(f'Yuou guess it in {guess_count} tries.')
+    
+
+        
